40d0e0bac97800f319728287291057df49f70277
sanch27 <sanchaliannavarapu@gmail.com>
2025-03-12 11:16:41 +0530

    if else condtion

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import "fmt" // importing the formatted output
 func main () { // entry point of the project
 	fmt.Println(" Hello World ")  // ln is used to print the data added in it directly
 
+	//Variables
 	//Assigment operator (variable should be declared to the value)
 	var name string = "Sanchali"
  	var age int = 22
@@ -11,7 +12,12 @@ func main () { // entry point of the project
 
 	//Short declaration operator ( variable declation is not needed )
 	batsman := "David Warner"
-	runs := 34
+	runs := 134
 	fmt.Printf("Batsmen: %s\nRuns: %d\n", batsman, runs)
+
+	//if else (condtional statements)
+	if runs >= 50 {
+		fmt.Println("Half-century!")
+	} else { fmt.Println("Not a half-century!") }	
 
 }
