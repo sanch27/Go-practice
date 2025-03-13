08e2f62b9b024e8afe8b0ee28eaac49723507caf
sanch27 <sanchaliannavarapu@gmail.com>
2025-03-13 10:47:53 +0530

    Homecontroller db is added to store the data

--- controllers/homeController.go
+++ controllers/homeController.go
@@ -2,13 +2,20 @@ package controllers
 
 import (
 	"fmt"
+	"go-tutorial/database"
+	"go-tutorial/models"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func GetHome(c *fiber.Ctx) error {
 	fmt.Println("Home Controller")
+	db := database.DB
+	var users []models.User
+	db.Find(&users)
+
 	return c.Render("index", fiber.Map{
 		"Title": "Home",
+		"Users": users,
 	})
 }
